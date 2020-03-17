50370eb17bb52d94c86574779c3362e7b59d0367
divyanshu <guptadivyanshu2012@gmail.com>
2020-03-18 01:40:16 +0530

    done with basic logic

--- shortener/models.py
+++ shortener/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
-from .utils import code_generator
+from .utils import create_shortcode
 # Create your models here.
+
+class shortURLManager(models.Model):
+	def all(self, *args, **kwargs):
+		qs = super(shortURLManager, self).all(*args, **kwargs)
+		qs = qs.filter(active = False)
+		return qs
+
 class shortURL(models.Model):
+
 	url = models.CharField(max_length = 220, )
 	shortcode = models.CharField(max_length = 15, blank=True, unique = True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default = True)
 
 	def save(self, *args, **kwargs):
 		if self.shortcode is None or self.shortcode == "":
-			self.shortcode = code_generator()
+			self.shortcode = create_shortcode(self)
 		super(shortURL, self).save(*args, **kwargs)
 
 	def __str__(self):
--- shortener/utils.py
+++ shortener/utils.py
@@ -1,5 +1,16 @@
 import random
 import string
 
-def code_generator(size=6, char=string.ascii_lowercase + string.digits):
+# from shortener.models import shortURL
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
+
+
+def create_shortcode(instance, size=6):
+	new_code = code_generator(size=size)
+	klass = instance.__class__
+	qs = klass.objects.filter(shortcode=new_code).exists()
+	if qs:
+		return create_shortcode(instance, size=size)
+	return new_code
